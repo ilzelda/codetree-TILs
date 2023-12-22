adc8990e5117c1c6df5bb904ae03f870bbe8ee93
ilzelda <51945422+ilzelda@users.noreply.github.com>
2023-12-22 22:54:43 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231222/다양하게 출력/print-in-variety.cpp
+++ 231222/다양하게 출력/print-in-variety.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     cout<<"Total days in Year\n"<<365<<endl;
-    cout<<"Circumference rate\n"<<3.1415926535<<endl;
+    cout<<"Circumference rate\n"<<"3.1415926535"<<endl;
     return 0;
 }
