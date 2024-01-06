f8019fe42c686665e21918dcf11d568b7dfaef0f
ilzelda <51945422+ilzelda@users.noreply.github.com>
2024-01-06 17:58:23 +0900

    Title: 두 실수의 곱, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240106/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+
+    float a = 5.26;
+    float b = 8.27;
+    
+    cout <<fixed <<setprecision(3)<< a*b;
+
+    return 0;
+}
