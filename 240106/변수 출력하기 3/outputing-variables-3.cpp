123494ea042c0d0b7025664cee74080bc76ae15a
ilzelda <51945422+ilzelda@users.noreply.github.com>
2024-01-06 17:48:07 +0900

    Title: 변수 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240106/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+
+    int a = 1;
+    int b = 2;
+    char c = 'C';
+
+    cout << a << "->" << b << "->" <<c; 
+
+    return 0;
+}
