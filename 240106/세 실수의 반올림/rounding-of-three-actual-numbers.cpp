ef0923d6ccb4bc6b3fb19693fa570d83f01960e6
ilzelda <51945422+ilzelda@users.noreply.github.com>
2024-01-06 18:22:18 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240106/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+    cout << fixed;
+    cout.precision(3);
+    double n;
+    cin >> n;
+    cout<< n << endl;
+
+    cin >> n;
+    cout<< n << endl;
+
+    cin >> n;
+    cout<< n << endl;
+
+    return 0;
+}
