ceb3d8b8767b0df1231314953f775eb6ce85ca06
ilzelda <51945422+ilzelda@users.noreply.github.com>
2024-01-06 18:59:31 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240106/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+    int a, b, c;
+    cin >> a ;
+    cin.get();
+    cin >> b;
+    cin.get();
+    cin >> c;
+    
+    cout << "010" << '-' << c << '-' << b;
+    return 0;
+}
