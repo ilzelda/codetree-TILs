c028adcbdbb207a1978a0ca0bbd2a20073fba57d
ilzelda <51945422+ilzelda@users.noreply.github.com>
2025-10-02 22:52:26 +0900

    Title: 사각형 채우기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251002/사각형 채우기/rectangle-fill.py
+++ 251002/사각형 채우기/rectangle-fill.py
@@ -10,6 +10,9 @@ def recur(n):
     a = recur(n-1) if mem[n-1] == -1 else mem[n-1]
     b = recur(n-2) if mem[n-2] == -1 else mem[n-2]
 
+    mem[n-1] = a
+    mem[n-2] = b
+    
     return a + b
 
 print(recur(N) % 10007)
