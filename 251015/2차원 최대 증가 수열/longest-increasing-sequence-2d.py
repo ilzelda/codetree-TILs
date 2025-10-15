31654904ee3d78fc8c321df4c59d50941c68f11a
ilzelda <51945422+ilzelda@users.noreply.github.com>
2025-10-15 10:20:28 +0900

    Title: 2차원 최대 증가 수열, Time: 78ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 251015/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -0,0 +1,22 @@
+N, M = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(N)]
+
+# Please write your code here.
+dp = [ [1] * M for _ in range(N)] # i,j 까지 왔을 때 최대 점프 횟수를 저장
+
+for i in range(N):
+    for j in range(M):
+        num = grid[i][j]
+
+        for si in range(i):
+            for sj in range(j):
+                if grid[si][sj] < num :
+                    dp[i][j] = max(dp[i][j], dp[si][sj] + 1)
+
+# for row in dp:
+#     print(row)
+
+ans = 0
+for row in dp:
+    ans = max( ans, max(row) )
+print(ans)
