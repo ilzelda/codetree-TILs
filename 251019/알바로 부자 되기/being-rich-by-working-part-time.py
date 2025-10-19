2880f82576ddac95fd0518cb7750bf1341beba32
ilzelda <51945422+ilzelda@users.noreply.github.com>
2025-10-19 12:07:51 +0900

    Title: 알바로 부자 되기, Time: 81ms, Memory: 17MB, Status: Passed - Codetree

--- 251019/알바로 부자 되기/being-rich-by-working-part-time.py
+++ 251019/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -13,17 +13,18 @@ for _ in range(N):
     s, e, p = map(int, input().split())
     jobs.append(Job(s,e,p))
 
-dp = [ 0 for _ in range(len(jobs))] # i번째 알바까지 고려했을 때 최대 돈
+dp = [ 0 for _ in range(len(jobs))] # i번째 알바를 선택했을 때 최대 돈
 dp[0] = jobs[0].pay
 
 def is_overlapped(j1, j2):
-    return j1.start <= j2.start <= j1.end
+    return j1.start <= j2.start <= j1.end 
 
 for i in range(1,len(jobs)):
-    d = 1
-    while i-d >= 0 and is_overlapped(jobs[i-d], jobs[i]) :
-        d+=1
-    dp[i] = max(dp[i-1], dp[i-d] + jobs[i].pay)
+    for j in range(i):    
+        if is_overlapped(jobs[j], jobs[i]) : 
+            dp[i] = max(dp[i], jobs[i].pay)
+        else:
+            dp[i] = max(dp[i], dp[j] + jobs[i].pay)
 
 # print('dp : ', dp)
 
