077e75e963a753c0e751869d93e0ff153070cb93
ilzelda <51945422+ilzelda@users.noreply.github.com>
2025-10-25 10:21:27 +0900

    Title: 1, 2, 5 더하기, Time: 49ms, Memory: 17MB, Status: Passed - Codetree

--- 251025/1, 2, 5 더하기/1-2-5-plus.py
+++ 251025/1, 2, 5 더하기/1-2-5-plus.py
@@ -1,7 +1,7 @@
 N = int(input())
 coins = [1,2,5]
 # Please write your code here.
-dp = [-1 for _ in range(N+1)] # i를 만드는 가짓수
+dp = [-1 for _ in range(1000+1)] # i를 만드는 가짓수
 
 dp[0] = 1
 dp[1] = 1
@@ -18,4 +18,4 @@ for i in range(3,N+1):
 
 # print(dp)
 
-print(dp[N])
+print(dp[N] % 10007)
