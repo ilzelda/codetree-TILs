845e90824423259a49b26db9d3d491ef7e9ea59a
ilzelda <51945422+ilzelda@users.noreply.github.com>
2025-10-28 10:48:44 +0900

    Title: 최대 동전 거슬러주기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251028/최대 동전 거슬러주기/max-coin-change.py
+++ 251028/최대 동전 거슬러주기/max-coin-change.py
@@ -2,25 +2,32 @@ N, M = map(int, input().split())
 coins = list(map(int, input().split()))
 
 # Please write your code here.
-dp = [-1 for _ in range(10000+1)] # i금액의 최대 동전개수
+dp = [-1 for _ in range(M+1)] # i금액의 최대 동전개수
 
 dp[0] = 0
 
-def recur(val):
-    global dp
+# def recur(val):
+#     global dp
 
-    if val < 0 : return -1
-    if dp[val] != -1 : return dp[val]
+#     if val < 0 : return -1
+#     if dp[val] != -1 : return dp[val]
 
-    res = 0
+#     res = 0
 
+#     for c in coins:
+#         res = max( res, recur(val-c) + 1)
+#     dp[val] = res
+
+#     return res
+
+# print(recur(M))
+
+for i in range(M+1):
     for c in coins:
-        res = max( res, recur(val-c) + 1)
-    dp[val] = res
+        j = i-c
+        if j < 0 : continue
 
-    return res
-
-print(recur(M))
+        dp[i] = max(dp[i], dp[j]+1)
 
 # for i in range(M+1):
 #     print(f"{i:2d}", end= ' ')
