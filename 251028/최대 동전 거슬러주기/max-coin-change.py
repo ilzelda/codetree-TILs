43f6317567557902f24b2935573c27de17e469c0
ilzelda <51945422+ilzelda@users.noreply.github.com>
2025-10-28 10:43:11 +0900

    Title: 최대 동전 거슬러주기, Time: 130ms, Memory: 23MB, Status: Runtime Error - Codetree

---
+++ 251028/최대 동전 거슬러주기/max-coin-change.py
@@ -0,0 +1,30 @@
+N, M = map(int, input().split())
+coins = list(map(int, input().split()))
+
+# Please write your code here.
+dp = [-1 for _ in range(M+1)] # i금액의 최대 동전개수
+
+dp[0] = 0
+
+def recur(val):
+    global dp
+
+    if val < 0 : return -1
+    if dp[val] != -1 : return dp[val]
+
+    res = 0
+
+    for c in coins:
+        res = max( res, recur(val-c) + 1)
+    dp[val] = res
+
+    return res
+
+print(recur(M))
+
+# for i in range(M+1):
+#     print(f"{i:2d}", end= ' ')
+# print()
+# for v in dp:
+#     print(f"{v:2d}", end = ' ' )
+# print()
