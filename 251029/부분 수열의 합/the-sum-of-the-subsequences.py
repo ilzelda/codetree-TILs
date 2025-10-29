20ec43d026b3c28d8b45d00cc650a69e64319c7b
ilzelda <51945422+ilzelda@users.noreply.github.com>
2025-10-29 10:58:45 +0900

    Title: 부분 수열의 합, Time: 147ms, Memory: 24MB, Status: Passed - Codetree

--- 251029/부분 수열의 합/the-sum-of-the-subsequences.py
+++ 251029/부분 수열의 합/the-sum-of-the-subsequences.py
@@ -2,33 +2,31 @@ N, M = map(int, input().split())
 A = list(map(int, input().split()))
 
 # Please write your code here.
-res = False
+memo = [ [-1]*(M+1) for _ in range(N) ] # -1 : none 0 : false 1: true
 
-def recur(i, left, val):
-    global res
+def recur(i, val):
+    global memo
     
-    if i == len(A):
-        if val < 0 : 
-            return
+    if val < 0 : 
+        return 0
+    
+    if i >= N:
         if val == 0 : 
-            res = True
-            print("Yes")
-            exit()
-            return
-        return
+            return 1
+        if val > 0 :
+            return 0
 
-    # 포함
-    left.append(A[i])
-    recur(i+1, left, val-A[i])
+    if memo[i][val] < 0:
+        # 포함
+        res1 = recur(i+1, val-A[i])
 
-    # 미포함
-    left.pop()
-    recur(i+1, left, val)
+        # 미포함
+        res2 = recur(i+1, val)
+    
+        memo[i][val] = 1 if res1 == 1 or res2 == 1 else 0
 
+    return memo[i][val]
 
-
-
-recur(0, [], M)
-
-ans = "Yes" if res else "No"
-print("No")
+ans = recur(0, M)
+res = "Yes" if ans == 1 else "No"
+print(res)
