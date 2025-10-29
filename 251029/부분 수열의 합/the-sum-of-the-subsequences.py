5d8e05096fd5d024b8484df89665536d0875af47
ilzelda <51945422+ilzelda@users.noreply.github.com>
2025-10-29 10:16:29 +0900

    Title: 부분 수열의 합, Time: 545ms, Memory: 80MB, Status: Memory Limit Exceed - Codetree

---
+++ 251029/부분 수열의 합/the-sum-of-the-subsequences.py
@@ -0,0 +1,31 @@
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+
+# Please write your code here.
+res = False
+
+def recur(i, left, val):
+    global res
+    
+    if i == len(A):
+        if val < 0 : 
+            return
+        if val == 0 : 
+            res = True
+            return
+        return
+
+    # 포함
+    left.append(A[i])
+    recur(i+1, left, val-A[i])
+
+    # 미포함
+    recur(i+1, left, val)
+
+
+
+
+recur(0, [], M)
+
+ans = "Yes" if res else "No"
+print(ans)
