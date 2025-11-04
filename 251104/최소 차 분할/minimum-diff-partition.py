65eba3a54d714439dad9d8de8177b79cd7a4a69f
ilzelda <51945422+ilzelda@users.noreply.github.com>
2025-11-04 10:44:40 +0900

    Title: 최소 차 분할, Time: 75ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 251104/최소 차 분할/minimum-diff-partition.py
@@ -0,0 +1,24 @@
+N = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+total = sum(arr)
+
+INT_MAX = 1 << 30
+ans = INT_MAX
+
+dp = [False for _ in range(1000 * 100 + 1)] # i를 만들 수 있는지
+dp[0] = True
+
+for i in range(0, len(dp)):
+    if dp[i]:
+        ans = min(ans, abs(total - (2*i)))
+
+        for n in arr:
+            if i+n < len(dp) : dp[i+n] = True
+
+print(ans)
+
+
+
+
