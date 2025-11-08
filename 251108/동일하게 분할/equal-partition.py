a2d1cdee5ae8d22c6e91f7c374824593211290b3
ilzelda <51945422+ilzelda@users.noreply.github.com>
2025-11-08 14:34:13 +0900

    Title: 동일하게 분할, Time: 56ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 251108/동일하게 분할/equal-partition.py
@@ -0,0 +1,20 @@
+N = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+total = sum(arr)
+
+dp = [False for _ in range(total+1)] # sumA=i를 만들 수 있는가
+dp[0]= True
+
+for n in arr:
+    for i in range(len(dp))[::-1]:
+        if i-n >=0: dp[i] = True if dp[i-n] else False
+
+if dp[int(total/2)] : ans = "Yes" 
+else : ans = "No"
+
+print(ans)
+
+
+
