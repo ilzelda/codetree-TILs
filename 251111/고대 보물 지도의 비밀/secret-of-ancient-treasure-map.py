e4ac4fda2b7dea4666125c017f6f9d9f050390a2
ilzelda <51945422+ilzelda@users.noreply.github.com>
2025-11-11 10:16:36 +0900

    Title: 고대 보물 지도의 비밀, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251111/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
+++ 251111/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
@@ -2,24 +2,28 @@ N, K = map(int, input().split())
 numbers = list(map(int, input().split()))
 
 # Please write your code here.
-INT_MIN = (1<<30) * -1
+INT_MIN = (1<<20) * -1
 
-dp = [ [INT_MIN]*(K+1) for _ in range(N+1)] # i번째까지 선택했을 때, j개의 음수 선택상태에서의, 최대합
+dp = [ [INT_MIN]*(K+1) for _ in range(N)] # i번째까지 선택했을 때, j개의 음수 선택상태에서의, 최대합
 
-dp[0][0] = 0
+if numbers[0] >= 0 :
+    dp[0][0] = numbers[0]
+else : 
+    dp[0][1] = numbers[0]
 
-for i in range(1,N+1):
-    if numbers[i-1] >= 0:
+for i in range(1,N):
+    if numbers[i] >= 0:
         for j in range(K+1):
-            dp[i][j] = max(dp[i-1][j] + numbers[i-1], numbers[i-1])
+            dp[i][j] = max(dp[i-1][j] + numbers[i], numbers[i])
     else:
         for j in range(1,K+1):
-            dp[i][j] = dp[i-1][j-1] + numbers[i-1]
+            dp[i][j] = max(dp[i-1][j-1] + numbers[i], numbers[i])
 
 ans = INT_MIN
 for row in dp:
     ans = max(ans, max(row))
 
+# print("INTMIN:",INT_MIN)
 # for row in dp : 
 #     for n in row:
 #         if n == INT_MIN:
