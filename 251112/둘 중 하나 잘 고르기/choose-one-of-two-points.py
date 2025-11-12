5ee29b0c74c86aa5df0f091cc5244e7b1116451c
ilzelda <51945422+ilzelda@users.noreply.github.com>
2025-11-12 09:58:53 +0900

    Title: 둘 중 하나 잘 고르기, Time: 59ms, Memory: 17MB, Status: Passed - Codetree

--- 251112/둘 중 하나 잘 고르기/choose-one-of-two-points.py
+++ 251112/둘 중 하나 잘 고르기/choose-one-of-two-points.py
@@ -10,16 +10,21 @@ for _ in range(2 * N):
 # Please write your code here.
 INT_MIN = (1<<30) * -1
 dp = [ [INT_MIN] * (N+1) for _ in range(2*N +1) ] # i번째까지ㅏ j개의 빨간색을 선택했을 때 최대 합 
-dp[0][0] = 0
+for j in range(N+1):
+    dp[0][j] = 0
 
+dp[1][0] = blue[1]
+dp[1][1] = red[1]
 
-for i in range(1, 2*N + 1):
-    for j in range(1, N+1):
+for i in range(2, 2*N + 1):
+    for j in range(N+1):
+        if j > i : break
+
         dp[i][j] = max(dp[i-1][j-1] + red[i], dp[i-1][j] + blue[i], dp[i][j])
 
-for row in dp:
-    for n in row:
-        print(f"{n:2d}" if n >=0 else ' X' , end= ' ')
-    print()
+# for row in dp:
+#     for n in row:
+#         print(f"{n:2d}" if n >=0 else ' X' , end= ' ')
+#     print()
 
 print(dp[-1][-1])
