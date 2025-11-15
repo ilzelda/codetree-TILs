14165d0045a67736ad657f3468e87b87310e2fde
ilzelda <51945422+ilzelda@users.noreply.github.com>
2025-11-15 10:55:14 +0900

    Title: M개의 구간을 선택하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251115/M개의 구간을 선택하기/select-m-sections.py
@@ -0,0 +1,32 @@
+N, M = map(int, input().split())
+numbers = list(map(int, input().split()))
+
+# Please write your code here.
+# 구간을 지금까지 i개 선택했을 때, j번째 까지의 최대합
+INT_MIN = (1<<30) * -1
+dp = [ [INT_MIN]*(N) for _ in range(M+1)]
+for j in range(N):
+    dp[0][j] = 0
+
+dp[1][0] = numbers[0]
+for j in range(1,N):
+    dp[1][j] = max(dp[1][j-1], dp[1][j-1] + numbers[j] )
+
+for i in range(2, M+1):
+    for j in range(2*(i-1), N):
+        
+        for k in range(j-1):
+            for l in range(k+2,j+1):
+                dp[i][j] = max(dp[i][j], 
+                                dp[i-1][k] + sum(numbers[l:j+1]))
+
+# for row in dp:
+#     for n in row:
+#         if n < -500 * 1000:
+#             print('  x',end='')
+#         else:
+#             print(f"{n:3d}",end='')
+#     print()
+
+print(max(dp[-1]))
+        
